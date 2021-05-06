7e8a8667511cff3ba33493cb599a0a1fb61aba52
AaronTho <aamith@gmail.com>
2021-05-05 22:10:29 -0600

    added arithmetic operations

---
+++ arithmetic_operators.py
@@ -0,0 +1,20 @@
+x = 9
+y = 3
+result = 9 / 3
+# result = x + y
+# result = int(x / y) will return and integer not a float as is the default.
+# exponents result = x ** y will return x to the power of y.
+
+# Order of operation:
+# B brackets
+# E exponents
+# D division
+# M multiplication
+# A addition
+# S subtraction
+# result = (x * y) * 2
+#
+#
+
+
+print(result)
---
+++ inputs.py
@@ -0,0 +1,3 @@
+name = input('Name: ')
+age = imput('Age: ')
+print "hello", name, 'you are', age, 'years old')
